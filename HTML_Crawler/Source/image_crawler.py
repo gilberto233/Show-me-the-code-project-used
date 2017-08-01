b387876aa8b43220b0e929e04f84461a865cf632
gilberto233 <flyashelike@outlook.com>
2017-08-02 00:29:30 +0800

    Update image_crawler.py

--- HTML_Crawler/Source/image_crawler.py
+++ HTML_Crawler/Source/image_crawler.py
@@ -1,13 +1,20 @@
 """
-
+Script download the pictures in specify site which only available in 
+tieba.baidu.com. What's more, the crawler finishes its work under the
+asist of multiprocessor.
 """
 from sys import argv
 from multiprocessing.dummy import Pool
 import urllib.request, re, os
 
 class image_crawler():
-
+    
     def __init__(self, param):
+        """
+        Recieve three parameters: Website's URL; Storage path in your hardware;
+        Number of your device's core.
+        param: Input stream
+        """
         if ( len( param ) < 3 ):
             print( 'Usage: python crawler.py $URL $dest_folder $num_of_threads' )
             exit()
@@ -22,6 +29,11 @@ class image_crawler():
         self.__agent__()
 
     def __agent__(self):
+        """
+        Agent will take care of the remain download task by analysing
+        available image URL and hand in to __download_image__() function.
+        return: None
+        """
         html_buffer = self.__fetch_html__()
         download_list = []
 
@@ -36,6 +48,11 @@ class image_crawler():
         else: exit()
 
     def __fetch_html__(self):
+        """
+        Fetch the website's hyper-text. Return the html file in string
+        format. Function with timeout consideration.
+        return: web_in_html.read()
+        """
         print( 'Fetching html.' )
 
         for i in range(5):
@@ -48,6 +65,11 @@ class image_crawler():
         return None
 
     def __fetch_image_url__(self, content):
+        """
+        Obtain available image URL in the html file by regular expression
+        analysing.
+        return: available_object
+        """
         print( 'Fetching image object url.' )
         pattern = '<img pic_type=... class="BDE_Image" src="(.*?)"'
 
@@ -56,6 +78,10 @@ class image_crawler():
         return available_object
 
     def __download_image__(self, list):
+        """
+        Bind the __download__() function with multiprocessor API.
+        return: None
+        """
         print( 'Downloading.' )
 
         download_info = [ ( single, os.path.basename( single ) ) for single in list ]
@@ -63,6 +89,11 @@ class image_crawler():
         pool.map( self.__download__, download_info )
 
     def __download__(self, dparam):
+        """
+        Download the specify image in website and storage them into local
+        filesystem. Function with timeout consideration.
+        return: None
+        """
         ( url, filename ) = dparam
 
         for i in range(6):
