ef44759718c805d96c5d052409323eedb0f35976
gilberto233 <flyashelike@outlook.com>
2017-08-07 17:26:33 +0800

    Create main.py

---
+++ Bing_STT_API/Source/main.py
@@ -0,0 +1,17 @@
+"""
+The main entry of STT project.
+"""
+#!/usr/bin/env python
+import Record, STT_API
+
+class function_entry():
+
+    def __init__(self):
+        self.__start__()
+
+    def __start__(self):
+        instance = Record()
+        STT_API( instance.get_file_name() )
+
+if __name__ == '__main__':
+    instance = function_entry()
